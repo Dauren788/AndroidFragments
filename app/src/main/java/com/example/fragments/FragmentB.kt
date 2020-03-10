2fdd531017f89358c5304b06cf4989db2ed334dc
Dauren <ddauren788@gmail.com>
2020-03-10 17:50:35 +0600

    add

---
+++ app/src/main/java/com/example/fragments/FragmentB.kt
@@ -0,0 +1,23 @@
+package com.smartherd.multiplescreensupport
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.fragments.R
+
+
+class FragmentB : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        return inflater.inflate(R.layout.fragment_b, container, false)
+    }
+
+    fun displayDetails(title: String, description: String) {
+
+//        txvTitle.text = title
+//        txvDescription.text = description
+    }
+}
