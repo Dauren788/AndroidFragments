c671aa9d0847b2d0bb55f17def6c93925f4cee28
Dauren <ddauren788@gmail.com>
2020-03-28 13:27:05 +0600

    Init

--- app/src/main/java/com/example/fragments/LoginActivity.java
+++ app/src/main/java/com/example/fragments/LoginActivity.java
@@ -1,26 +1,100 @@
 package com.example.fragments;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.smartherd.multiplescreensupport.MainActivity;
 
-public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+public class LoginActivity extends AppCompatActivity implements MainActivity.LogOutListener{
+
+    static Context context ;
+    FragmentManager fragmentManager;
+    Button log;
+    final String SAVED_TEXT = "initial";
+    EditText email , password;
+    SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        Button button = (Button)findViewById(R.id.button);
-        button.setOnClickListener(this);
+       // log.setOnClickListener(this);
+
+        context = this;
+        Log.e("my" , "createMain");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        sharedPreferences = getPreferences(MODE_PRIVATE);
+        String savedEmail = sharedPreferences.getString("email" , "");
+        String savedPassword = sharedPreferences.getString("password" , "");
+        savedEmail = savedEmail.trim();
+        savedPassword = savedPassword.trim();
+
+        if( (!savedEmail.isEmpty()) && (!savedPassword.isEmpty()) ){
+            Log.e("my" , "toMainPage " + savedEmail + " pass " + savedPassword);
+            this.toMainPage();
+        }
+
+        log = (Button) findViewById(R.id.button);
+        email = (EditText) findViewById(R.id.email);
+        password = (EditText) findViewById(R.id.password);
     }
+
+    public void log(View v){
+        this.save();
+        String emailText = email.getText().toString().trim();
+        String passwordText = password.getText().toString().trim();
+        if(emailText.isEmpty() || passwordText.isEmpty()){
+            Toast.makeText(this ,"Can not be empty email or password!" ,Toast.LENGTH_LONG).show();
+        }else{
+            Toast.makeText(this, "email:" + emailText + "password:" + passwordText , Toast.LENGTH_LONG).show();
+            this.toMainPage();
+        }
+    }
+    public void toMainPage(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+    public void save(){
+        sharedPreferences = getPreferences(MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("email" , email.getText().toString());
+        editor.putString("password" , password.getText().toString());
+        editor.apply();
+    }
+
     @Override
-    public void onClick(View view) {
-        Intent i;
-        i = new Intent(this, MainActivity.class);
-        startActivity(i);
+    protected void onDestroy() {
+        super.onDestroy();
     }
+
+
+    public void out() {
+        Log.e("my" , "MainActivity OUT");
+        sharedPreferences = getPreferences(MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("email" , "");
+        editor.putString("password", "");
+        editor.apply();
+    }
+
+   //Override
+   //ublic void onClick(View view) {
+   //   Intent i;
+   //   i = new Intent(this, MainActivity.class);
+   //   startActivity(i);
+   //
+   //
+   //
 }
--- app/src/main/java/com/example/fragments/MainActivity.kt
+++ app/src/main/java/com/example/fragments/MainActivity.kt
@@ -6,14 +6,26 @@ import android.support.v7.app.AppCompatActivity
 import android.view.View
 import com.example.fragments.R
 import kotlinx.android.synthetic.main.activity_main.*
+import android.R
+import android.util.Log
+import android.R
+import android.content.Context
+import android.support.v4.app.FragmentManager
+import android.R
+
+
+
 
 class MainActivity : AppCompatActivity(), MyCommunicator {
 
     private var mIsDualPane = false
+    var fragmentManager: FragmentManager? = null
+    var logOutListener: LogOutListener? = null
+    var context: Context? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(com.example.fragments.R.layout.activity_main)
 
 
         fragmentA.view?.setOnClickListener {
@@ -21,20 +33,33 @@ class MainActivity : AppCompatActivity(), MyCommunicator {
             startActivity(intent)
         }
 
-        val fragmentBView = findViewById<View>(R.id.fragmentB)
+        val fragmentBView = findViewById<View>(com.example.fragments.R.id.fragmentB)
         mIsDualPane = fragmentBView?.visibility == View.VISIBLE
     }
 
     override fun displayDetails(title: String, description: String) {
 
         if (mIsDualPane) {
-            val fragmentB = supportFragmentManager.findFragmentById(R.id.fragmentB) as FragmentB?
+            val fragmentB = supportFragmentManager.findFragmentById(com.example.fragments.R.id.fragmentB) as FragmentB?
             fragmentB?.displayDetails(title, description)
-        } else { 
+
+
+        } else {
             val intent = Intent(this, DetailActivity::class.java)
             intent.putExtra("title", title)
             intent.putExtra("description", description)
             startActivity(intent)
         }
+    }
+    fun logOut(v: View) {
+        Log.e("my", "Log out")
+        logOutListener = MainActivity.context as LogOutListener
+        logOutListener.out()
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+    interface LogOutListener {
+        fun out()
     }
 }
